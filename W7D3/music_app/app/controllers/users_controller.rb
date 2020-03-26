06aee968bc79571089cd0562167b446dc3b18984
xdeng9 <xdeng9@gmail.com>
2020-03-25 21:55:59 -0700

    Update classword

---
+++ W7D3/music_app/app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    def index
+        @users = User.all 
+        render :index 
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        if @user 
+            render :show 
+        else
+            @user.errors_full_messages
+        end
+    end
+
+    def new
+        @user = User.new()
+        render :new
+    end
+
+    def edit
+        @user = User.find_by(id: params[:id]) 
+        render :edit 
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            redirect_to users_url
+        else
+            render :new 
+        end
+    end
+
+    def destroy
+        @user = find_by(id: params[:id])
+        @user.destroy
+        redirect_to users_url 
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+end
